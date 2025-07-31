6964f44ae05416eb002b921676b0f02e0ec4f864
Revathi2006-cyber <veeraboyanirevathi@gmail.com>
2025-07-31 18:28:51 +0530

    Update config.py

--- config.py
+++ config.py
@@ -4,5 +4,5 @@
 EMERGENCY_CONTACT = "+918555893977"
 
 # Default emergency message (optional)
-DEFAULT_MESSAGE = "🚨 I'm in danger! Please help me immediately."
+DEFAULT_MESSAGE = "I'm in danger! Please help me immediately."
 
