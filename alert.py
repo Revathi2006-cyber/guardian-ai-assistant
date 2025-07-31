7908c89075df29c3928e5a81b6c7f624ae387278
Revathi2006-cyber <veeraboyanirevathi@gmail.com>
2025-07-31 18:28:09 +0530

    Update alert.py

--- alert.py
+++ alert.py
@@ -3,17 +3,17 @@ import pyautogui
 import time
 
 def send_whatsapp_alert(phone_number, message):
-    print(f"📲 Sending WhatsApp message instantly to {phone_number}...")
+    print(f"Sending WhatsApp message instantly to {phone_number}...")
 
     try:
         pywhatkit.sendwhatmsg_instantly(phone_number, message, wait_time=20, tab_close=False)
         
         # Wait extra time to ensure the message box is ready
-        print("⏳ Waiting before pressing Enter...")
+        print("Waiting before pressing Enter...")
         time.sleep(20)
 
         pyautogui.press('enter')  # Send message
-        print("✅ Message sent automatically!")
+        print("Message sent automatically!")
 
     except Exception as e:
-        print(f"❌ Error sending message: {e}")
+        print(f"Error sending message: {e}")
